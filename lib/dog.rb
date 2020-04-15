ca8a448e2c05b2223acb0b59ba8cb26e8e36ee57
Santiago Molinari <santomolinari@gmail.com>
2020-04-15 16:15:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,15 @@
 class Dog 
   
   def name=(dog_name)
-    this_dog_name = dog_name 
+    this_dogs_name = dog_name 
   end
   
   def name 
-    this_dog_name       #this method exposes data #from inside to the outside world.
+    this_dogs_name       #this method exposes data #from inside to the outside world.
   end 
   
-lassie = Dog.new
-lassie.name = "Lassie"
 
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
